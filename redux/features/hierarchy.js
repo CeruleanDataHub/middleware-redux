3b833aae6c2d3ec2f495d5af13b0360820c2ee46
Muhammad Salman <salmanmanekia@gmail.com>
2020-05-28 16:12:40 +0300

    Add hierarchy actions
    Co-authored-by: Miikka Alatalo <miikka.alatalo@houston-inc.com>
    Co-authored-by: Muhammad Salman <salmanmanekia@gmail.com>

---
+++ redux/features/hierarchy.js
@@ -0,0 +1,56 @@
+import { createAsyncThunk, createSlice, createAction } from '@reduxjs/toolkit';
+import { invokeRequest } from './request.js';
+import dispatchThunk from '../utils.js';
+
+export const getAllHierarchies = createAsyncThunk(
+  'hierarchy/get',
+  async (_, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'get', url: '/hierarchy/all' }))
+);
+
+export const getHierarchyTree = createAsyncThunk(
+  'hierarchy/tree',
+  async (_, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'get', url: '/hierarchy/tree' }))
+);
+
+export const addHierarchy = createAsyncThunk(
+  'hierarchy/add',
+  async (data, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'post', url: '/hierarchy/', data: data }))
+);
+
+export const deleteHierarchy = createAsyncThunk(
+  'hierarchy/delete',
+  async (id, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'delete', url: `/hierarchy/${id}` }))
+);
+
+export const editHierarchy = createAsyncThunk(
+  'hierarchy/update',
+  async (hierarchy, thunk) =>
+    await dispatchThunk(
+      thunk,
+      invokeRequest({ method: 'put', url: `/hierarchy/${hierarchy.id}`, data: { name: hierarchy.name } })
+    )
+);
+
+export const setHierarchyTree = createAction('hierarchy/set');
+
+export const hierarchiesSlice = createSlice({
+  name: 'hierarchies',
+  initialState: { error: undefined },
+  extraReducers: {
+    [getAllHierarchies.fulfilled]: (state, action) => {
+      state.all = action.payload.body;
+    },
+    [getHierarchyTree.fulfilled]: (state, action) => {
+      state.tree = action.payload.body;
+    },
+    [deleteHierarchy.rejected]: (state, action) => {
+      return action;
+    },
+    [setHierarchyTree]: (state, action) => {
+      state.tree = action.payload;
+    },
+  },
+});
+export const { saveHierarchies } = hierarchiesSlice.actions;
+
+export default hierarchiesSlice.reducer;
