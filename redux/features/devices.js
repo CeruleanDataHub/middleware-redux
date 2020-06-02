1677750fcd34b2ee1aab6bc8da401812ad0dace2
Muhammad Salman <salmanmanekia@gmail.com>
2020-06-02 12:17:05 +0300

    Add test case for hierarchy
    Co-authored-by: Miikka Alatalo <miikka.alatalo@houston-inc.com>
    Co-authored-by: Muhammad Salman <salmanmanekia@gmail.com>

--- redux/features/devices.js
+++ redux/features/devices.js
@@ -1,33 +1,33 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { invokeRequest } from './request.js';
-import dispatchThunk from '../utils.js';
+import dispatchThunk from '../utils.js';
 
 export const getAllDevices = createAsyncThunk(
-    'devices/get',
-    async (_, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'get', url: '/api/v1/device/all' }))
+  'devices/get',
+  async (_, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'get', url: '/device/all' }))
 );
 
 export const getTwin = createAsyncThunk(
-    'twin/get',
-    async (id, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'get', url: `/api/v1/twin/${id}` }))
+  'twin/get',
+  async (id, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'get', url: `/twin/${id}` }))
 );
 
 export const updateTwin = createAsyncThunk(
-    'twin/update',
-    async (data, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'post', url: `/api/v1/twin/update`, data: data }))
+  'twin/update',
+  async (data, thunk) => await dispatchThunk(thunk, invokeRequest({ method: 'post', url: `/twin/update`, data: data }))
 );
 
 export const devicesSlice = createSlice({
-    name: 'devices',
-    initialState: { error: undefined },
-    extraReducers: {
-        [getAllDevices.fulfilled]: (state, action) => {
-            state.all = action.payload.body;
-        },
-        [getTwin.fulfilled]: (state, action) => {
-            state.twin = action.payload.body;
-        }
+  name: 'devices',
+  initialState: { error: undefined },
+  extraReducers: {
+    [getAllDevices.fulfilled]: (state, action) => {
+      state.all = action.payload.body;
     },
+    [getTwin.fulfilled]: (state, action) => {
+      state.twin = action.payload.body;
+    },
+  },
 });
 
 export const { saveDevices } = devicesSlice.actions;
