d6a7b1e43b5af7a299d1793f50539d70ad4e5e0a
Tommi Soikkeli <tommi.soikkeli@houston-inc.com>
2020-10-01 13:39:24 +0300

    Create util function for users output

--- redux/utils.js
+++ redux/utils.js
@@ -1,8 +1,25 @@
 export default async function dispatchThunk(thunk, f) {
-    const response = await thunk.dispatch(f);
-    if (response.error) {
-        return thunk.rejectWithValue({ body: null, error: response.payload.error });
-    }
-    const { payload: { body }} = response;
-    return { body, error: null };
+  const response = await thunk.dispatch(f);
+  if (response.error) {
+    return thunk.rejectWithValue({ body: null, error: response.payload.error });
+  }
+  const {
+    payload: { body },
+  } = response;
+  return { body, error: null };
 }
+
+export const getUsersOutputMapper = (data) => {
+  return data.map(
+    ({ user_id, name, email, logins_count, last_login, blocked }, idx) => ({
+      id: idx,
+      userId: user_id,
+      name,
+      email: email || '',
+      logins: logins_count,
+      lastLogin: last_login,
+      blocked: blocked || false,
+      roles: [],
+    })
+  );
+};
